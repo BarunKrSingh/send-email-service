01a3695f206b35eda07b5d8263ea3c31599013ec
Barun <barun.singh@emc.com>
2016-05-30 19:10:42 +0530

    Added changes for email sender for sending e-mail body as HTML text

--- src/main/java/com/telefonica/email/EmailSender.java
+++ src/main/java/com/telefonica/email/EmailSender.java
@@ -1,11 +1,15 @@
 package com.telefonica.email;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 import com.telefonica.domain.EmailContext;
 import com.telefonica.messaging.RabbitMQReceiver;
@@ -18,22 +22,29 @@ public class EmailSender {
 	@Autowired
 	private JavaMailSender javaMailSender;
 
-	public void sendEmail(EmailContext emailContext) {
+	public void sendEmail(final EmailContext emailContext) {
 
 		try {
 
-			SimpleMailMessage mailMessage = new SimpleMailMessage();
+			//SimpleMailMessage mailMessage = new SimpleMailMessage();
 			
-			//mailMessage.setTo(emailContext.getTo());
-			//This is for test
-			mailMessage.setTo("barun.singh@emc.com");
+			MimeMessagePreparator preparator = new MimeMessagePreparator() {
+	            @Override
+	            public void prepare(MimeMessage mimeMessage) throws Exception {
+	               MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+	               
+	               //message.setTo(emailContext.getTo());
+	               message.setTo("barun.singh@emc.com");
+	               message.setFrom(emailContext.getFrom());
+	               message.setSubject(emailContext.getSubject());
+
+	               message.setText(emailContext.getBody(), true);
+	            }
+	         };			
+	         
+	         javaMailSender.send(preparator);
 			
-			mailMessage.setFrom(emailContext.getFrom());
-			mailMessage.setSubject(emailContext.getSubject());
-			mailMessage.setText(emailContext.getBody());
-			javaMailSender.send(mailMessage);
-			
-			logger.debug("Successfully sent the email to the user:{} with information on password change:",emailContext.getTo());
+			 logger.debug("Successfully sent the email to the user:{} with information on password change:",emailContext.getTo());
 
 		} catch (Exception e) {
 			logger.error("Exception occured sending email to:{} due to :{}", emailContext.getTo(), e);
